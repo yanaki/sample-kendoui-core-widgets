1f8ec42f6e95b24cdaa8811b91a715b402c55646
Yanaki Yanakiev <yanaki.yanakiev@telerik.com>
2014-09-10 10:52:24 -0500

    Lazy load google maps api

--- sample-kendoui-core-widgets/scripts/app.js
+++ sample-kendoui-core-widgets/scripts/app.js
@@ -1,29 +1,39 @@
 (function (global) {
-    var app = global.app = global.app || {};
+    global.$.getScript("https://www.google.com/jsapi").then(function () {
 
-    app.makeUrlAbsolute = function (url) {
-            var anchorEl = document.createElement("a");
-            anchorEl.href = url;
-            return anchorEl.href;
-        };
+        global.google.load('maps', '3.exp', {
+            other_params: 'sensor=true',
+            callback: function () {
+                var app = global.app = global.app || {};
 
-    document.addEventListener("deviceready", function () {
-        navigator.splashscreen.hide();
+                app.makeUrlAbsolute = function (url) {
+                    var anchorEl = document.createElement("a");
+                    anchorEl.href = url;
+                    return anchorEl.href;
+                };
 
-        app.changeSkin = function (e) {
-            var mobileSkin = "";
+                document.addEventListener("deviceready", function () {
+                    navigator.splashscreen.hide();
 
-            if (e.sender.element.text() === "Flat") {
-                e.sender.element.text("Native");
-                mobileSkin = "flat";
-            } else {
-                e.sender.element.text("Flat");
-                mobileSkin = "";
+                    app.changeSkin = function (e) {
+                        var mobileSkin = "";
+
+                        if (e.sender.element.text() === "Flat") {
+                            e.sender.element.text("Native");
+                            mobileSkin = "flat";
+                        } else {
+                            e.sender.element.text("Flat");
+                            mobileSkin = "";
+                        }
+
+                        app.application.skin(mobileSkin);
+                    };
+
+                    app.application = new kendo.mobile.Application(document.body, {
+                        layout: "tabstrip-layout"
+                    });
+                }, false);
             }
-
-            app.application.skin(mobileSkin);
-        };
-
-        app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout" });
-    }, false);
+        });
+    });
 })(window);
